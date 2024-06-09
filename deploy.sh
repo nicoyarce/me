13b9a359539f4898e093a6f9a4352f3b07a2b77a
Nicolas Oyarce <nicoyarce@gmail.com>
2024-06-09 13:38:12 -0400

    Fixes en vista movil

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,26 @@
 set -e
 
 rm -drf dist
+echo 'Removing old distribution'
 
 # build
 npm run build
+echo 'Building application'
 
 # navigate into the build output directory
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# echo 'www.example.com' > CNAM'
 
 git init
 git checkout -b main
 git add -A
 git commit -m "Deploy de app `date +'%Y-%m-%d %H:%M:%S'`"
+echo 'Committing'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:nicoyarce/me.git main:gh-pages
-
+echo 'Pushing'
 cd -
+echo 'Done'
