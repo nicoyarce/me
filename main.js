02a3690afb0789ff2b4dfd565cbd4ea3e86231c3
Nicolas Oyarce <nicoyarce@gmail.com>
2024-06-10 22:54:44 -0400

    Fixes visuales

--- main.js
+++ main.js
@@ -157,3 +157,17 @@ function repositionPhoto() {
 }
 
 animate();
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetElement = document.querySelector(this.getAttribute("href"));
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
